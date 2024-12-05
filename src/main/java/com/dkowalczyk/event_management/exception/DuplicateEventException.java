f7ed916cb1f5612bdc8e210b6c69fe83329d625a
dkowalczyk <d.kowalczyk@dituel.pl>
2024-12-05 16:16:51 +0100

    Revert "init commit"
    This reverts commit 86a1a8fbff88cd0d28f26c1f3b29042c7ba79475.

--- src/main/java/com/dkowalczyk/event_management/exception/DuplicateEventException.java
+++
@@ -1,7 +0,0 @@
-package com.dkowalczyk.event_management.exception;
-
-public class DuplicateEventException extends RuntimeException{
-    public DuplicateEventException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/dkowalczyk/event_management/repository/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dkowalczyk.event_management.repository;
-
-import com.dkowalczyk.event_management.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-    boolean existsByEventName(String eventName);
-}
